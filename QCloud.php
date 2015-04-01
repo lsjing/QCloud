47da099d9581d532f6f2971098ac15c2ebdb4948
xuef <boot_1@126.com>
2015-04-01 22:27:51 +0800

    del comment

--- QCloud.php
+++ QCloud.php
@@ -64,7 +64,6 @@ class QCloud {
 		}
 
 		$this->url .= 'signature=' . $this->doSignature($this->public_args);
-//$this->d($this->url, 0, 1);
 		$r = $this->curl_request($this->url);
 		$r = json_decode($r, true);
 		echo '<pre>';
@@ -97,14 +96,12 @@ class QCloud {
 		return $sign;
 	}
 
-	//参数1：访问的URL，参数2：post数据(不填则为GET)，参数3：提交的$cookies,参数4：是否返回$cookies
 	 function curl_request($url,$post='',$cookie='', $returnCookie=0){
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)');
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
-        //curl_setopt($curl, CURLOPT_REFERER, "http://XXX");
         if($post) {
             curl_setopt($curl, CURLOPT_POST, 1);
             curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));
@@ -130,16 +127,5 @@ class QCloud {
             return $data;
         }
 	}
-
-	public function d($arg, $a=false, $b=false){
-		echo '<pre>';
-		($a)?var_dump($arg):print_r($arg);
-		echo '</pre>';
-	    ($b)?exit:'';
-	}
-
 }
-
-
-
 ?>
